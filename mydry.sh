fab07e1a1359e995de8ab9c6e3afff75d263baa8
simm6688 <51709521+simm6688@users.noreply.github.com>
2019-12-28 19:11:05 +0800

    Update mydry.sh

--- mydry.sh
+++ mydry.sh
@@ -8,5 +8,5 @@ echo '修改机器名称'
 sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 echo '修改wifi名称'
 sed -i 's/OpenWrt/westlake5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-echo '切换内核'
-sed -i 's/4.14/4.19/g' target/linux/ipq40xx/Makefile
+#echo '切换内核'
+#sed -i 's/4.14/4.19/g' target/linux/ipq40xx/Makefile
